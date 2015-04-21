940d3cbbfaacb310e64c19a6d35f7ba5f1417892
Mick Ryan <mick@evilbits.io>
2015-04-21 11:23:54 -0700

    Adding stencil-utils as a default jspm package.

--- assets/config.js
+++ assets/config.js
@@ -15,16 +15,31 @@ System.config({
 
 System.config({
   "map": {
+    "asyncly/EventEmitter2": "github:asyncly/EventEmitter2@0.4.14",
     "babel": "npm:babel@4.7.16",
     "babel-runtime": "npm:babel-runtime@4.7.16",
+    "bigcommrece/stencil-utls": "github:bigcommerce/stencil-utils@0.0.1",
+    "core-js": "npm:core-js@0.8.4",
     "jquery": "github:components/jquery@2.1.3",
+    "lodash": "npm:lodash@3.7.0",
+    "github:bigcommerce/stencil-utils@0.0.1": {
+      "asyncly/EventEmitter2": "github:asyncly/EventEmitter2@0.4.14",
+      "jquery": "github:components/jquery@2.1.3",
+      "lodash": "npm:lodash@3.7.0"
+    },
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
     "npm:babel-runtime@4.7.16": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
+    "npm:core-js@0.8.4": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
     "npm:lodash@3.6.0": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:lodash@3.7.0": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     }
   }

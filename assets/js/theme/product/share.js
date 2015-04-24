1922a311c1c4fbe0776ce36dd833b87b3bc61bac
haubc <hau.nguyen@bigcommerce.com>
2015-04-24 13:49:42 -0700

    BIG-16044 Product events stubs for quantity, social media and wishlist

---
+++ assets/js/theme/product/share.js
@@ -0,0 +1,11 @@
+import utils from 'bigcommerce/stencil-utils'
+
+utils.events.on('product-share', (event, ele) => {
+    /*
+     Provide your overrides or custom code here
+     */
+});
+
+export default function (window) {
+
+}
---
+++ assets/js/theme/product/wishlist.js
@@ -0,0 +1,11 @@
+import utils from 'bigcommerce/stencil-utils'
+
+utils.events.on('product-wishlist', (event, ele) => {
+    /*
+     Provide your overrides or custom code here
+     */
+});
+
+export default function (window) {
+
+}

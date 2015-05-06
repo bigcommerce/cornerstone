7a6e9072e507c467cd62a32d825c4f6970ac174f
Mario Campa <mario.campa@bigcommerce.com>
2015-05-06 14:52:16 -0700

    BIG-16025 Cart content and JS

--- assets/js/theme/cart.js
+++ assets/js/theme/cart.js
@@ -1,7 +1,42 @@
 import PageManager from '../page-manager';
+import $ from 'jquery';
+import utils from 'bigcommerce/stencil-utils';
 
 export default class Cart extends PageManager {
     constructor() {
         super();
+
+        $(document.body).on('click', '[data-quantity-inc]', (event) => {
+            let itemId = $(event.target).attr('data-quantity-inc'),
+                el = $('#qty-' + itemId);
+
+            event.preventDefault();
+            this.changeQty(el, itemId, parseInt(el.html()) + 1);            
+        });
+
+        $(document.body).on('click', '[data-quantity-dec]', (event) => {
+            let itemId = $(event.target).attr('data-quantity-dec'),
+                el = $('#qty-' + itemId);
+
+            event.preventDefault();
+            this.changeQty(el, itemId, parseInt(el.html()) - 1);            
+        });
+    }
+
+    changeQty(el, itemId, qty) {
+        var oldQty = el.html();
+        el.html(qty);
+        // utils.events.emit('cart-update');
+        utils.remote.cart.cartItemUpdate(itemId, qty, (err, response) => {
+            console.log(response);
+            if (response.status === 'succeed') {
+                utils.remote.cart.cartContent({render_with: 'cart/content'}, (err, content) => {
+                    $('[data-cart-content]').html(content);
+                });
+            } else {
+                el.html(oldQty);
+                alert(response.errors.join('\n'));
+            }
+        });
     }
 }

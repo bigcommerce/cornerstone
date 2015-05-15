4e3173c0cb5ad1bfdf726e036c8648c65bb4041d
haubc <hau.nguyen@bigcommerce.com>
2015-05-15 11:54:11 -0700

    BIG-18027 Fixes from PR comments

--- assets/js/theme/cart.js
+++ assets/js/theme/cart.js
@@ -4,17 +4,20 @@ import utils from 'bigcommerce/stencil-utils';
 
 export default class Cart extends PageManager {
     loaded(next) {
-        utils.events.on('cart-item-update', (event, button) => {
-            let itemId = $(button).data('cart-update'),
+
+        // cart update
+        $('body').on('click', '.cart-update', (event) => {
+            let $target = $(event.currentTarget),
+                itemId = $target.data('cart-itemid'),
                 el = $('#qty-' + itemId),
                 oldQty = parseInt(el.text()),
                 newQty;
 
             event.preventDefault();
 
-            newQty = $(button).data('action') === 'inc' ? oldQty + 1 : oldQty - 1;
+            newQty = $target.data('action') === 'inc' ? oldQty + 1 : oldQty - 1;
             el.text(newQty);
-            utils.remote.cart.itemUpdate(itemId, newQty, (err, response) => {
+            utils.cart.itemUpdate(itemId, newQty, (err, response) => {
                 if (response.status === 'succeed') {
                     this.refreshContent();
                 } else {
@@ -24,12 +27,12 @@ export default class Cart extends PageManager {
             });
         });
 
-        utils.events.on('cart-item-remove', (event, el) => {
-            let itemId = $(el).data('cart-remove');
+        $('body').on('click', '.cart-remove', (event) => {
+            let itemId = $(event.currentTarget).data('cart-itemid');
 
             event.preventDefault();
 
-            utils.remote.cart.itemRemove(itemId, (err, response) => {
+            utils.cart.itemRemove(itemId, (err, response) => {
                 if (response.status === 'succeed') {
                     this.refreshContent();
                 } else {
@@ -42,7 +45,7 @@ export default class Cart extends PageManager {
     }
 
     refreshContent() {
-        utils.remote.cart.getContent({render_with: 'cart/content'}, (err, content) => {
+        utils.cart.getContent({render_with: 'cart/content'}, (err, content) => {
             $('[data-cart-content]').html(content);
         });
     }

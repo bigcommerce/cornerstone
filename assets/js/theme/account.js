314af4c375574ce099fdd319ab44cb99ce14bb82
Mick Ryan <mick@evilbits.io>
2015-05-22 17:36:30 -0700

    Add field helper to parse helper and insert partials. Also, fix for select elements not having a name element.

--- assets/js/theme/account.js
+++ assets/js/theme/account.js
@@ -1,7 +1,18 @@
 import PageManager from '../page-manager';
+import stateCountry from './common/state-country'
 
 export default class Account extends PageManager {
     constructor() {
         super();
+    }
+
+    loaded(next) {
+        let $stateElement = $('[data-label="State/Province"]');
+
+        if ($stateElement){
+            stateCountry($stateElement);
+        }
+
+        next();
     }
 }
--- assets/js/theme/auth.js
+++ assets/js/theme/auth.js
@@ -1,11 +1,8 @@
-import $ from 'jquery';
 import PageManager from '../page-manager';
-import utils from 'bigcommerce/stencil-utils';
-import _ from 'lodash';
+import stateCountry from './common/state-country'
 
 export default class Auth extends PageManager {
     constructor() {
-        this.$stateElement = $('[data-label="State/Province"]');
         super();
     }
 
@@ -14,55 +11,12 @@ export default class Auth extends PageManager {
      * @param next
      */
     loaded(next) {
-        $('select[data-label="Country"]').on('change', (event) => {
-            let countryName = $(event.currentTarget).val();
+        let $stateElement = $('[data-label="State/Province"]');
 
-            utils.country.getByName(countryName, (err, statesArray) => {
-                if (err) {
-                    alert('There was an error');
-                }
-                if (!_.isEmpty(statesArray)) {
-                    let $selectElement = this.changeInputToSelect();
-                    this.addOptions(statesArray, $selectElement);
-
-                } else {
-                    $('[data-label="State/Province"]').replaceWith(this.$stateElement.get(0));
-                }
-            })
-        });
+        if ($stateElement){
+            stateCountry($stateElement);
+        }
 
         next();
-    }
-
-    /**
-     * If there are no options from bcapp, a text field will be sent. This will create a select element to hold options after the remote request.
-     * @returns {jQuery|HTMLElement}
-     */
-    changeInputToSelect() {
-        let attrs;
-
-        attrs = _.transform(this.$stateElement.prop('attributes'), (result, item) => {
-            result[item.name] = item.value;
-            return result;
-        });
-
-        this.$stateElement.replaceWith(`<select id="${attrs.id}" data-label="${attrs['data-label']}" class="${attrs.class}" name="${attrs.name}"></select>`);
-        return $('[data-label="State/Province"]');
-    }
-
-    /**
-     * Adds the array of options from the remote request to the newly created select box.
-     * @param statesArray
-     * @param $selectElement
-     */
-    addOptions(statesArray, $selectElement) {
-        let container = [];
-        if (!_.isEmpty($selectElement)){
-            _.each(statesArray, (stateObj)  => {
-                container.push(`<option value="${stateObj.state}">${stateObj.state}</option>`);
-            });
-
-            $selectElement.html(container.join(' '));
-        }
     }
 }

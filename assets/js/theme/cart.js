57507946ee82a9ef6aa373c70e18114ffad8d972
Mario Campa <mario.campa@bigcommerce.com>
2015-06-08 12:43:44 -0700

    BIG-17751 Shipping Estimator + BIG-16026 Cart Coupon and Gift Certificate Box

--- assets/js/theme/cart.js
+++ assets/js/theme/cart.js
@@ -6,65 +6,107 @@ import utils from 'bigcommerce/stencil-utils';
 export default class Cart extends PageManager {
     loaded(next) {
 
-        let debounceTimeout = 400,
-            cartUpdate = _.debounce(($target) => {
-            let itemId = $target.data('cart-itemid'),
-                el = $('#qty-' + itemId),
-                oldQty = parseInt(el.text()),
-                newQty;
+        this.debounceTimeout = 400;
 
-            newQty = $target.data('action') === 'inc' ? oldQty + 1 : oldQty - 1;
-            el.text(newQty);
-            utils.api.cart.itemUpdate(itemId, newQty, (err, response) => {
-                if (response.status === 'succeed') {
-                    this.refreshContent();
-                } else {
-                    el.text(oldQty);
-                    alert(response.errors.join('\n'));
-                }
-            });
-        }, debounceTimeout),
-        cartRemoveItem = _.debounce((itemId) => {
-            utils.api.cart.itemRemove(itemId, (err, response) => {
-                if (response.status === 'succeed') {
-                    this.refreshContent();
-                } else {
-                    alert(response.errors.join('\n'));
-                }
-            });
-        }, debounceTimeout);
+        this.bindEvents();
 
+        next();
+    }
+    
+    cartUpdate($target) {
+        let itemId = $target.data('cart-itemid'),
+            el = $('#qty-' + itemId),
+            oldQty = parseInt(el.text()),
+            newQty;
+
+        newQty = $target.data('action') === 'inc' ? oldQty + 1 : oldQty - 1;
+        el.text(newQty);
+        console.log(this);
+        utils.api.cart.itemUpdate(itemId, newQty, (err, response) => {
+            if (response.data.status === 'succeed') {
+                this.refreshContent();
+            } else {
+                el.text(oldQty);
+                alert(response.data.errors.join('\n'));
+            }
+        });
+    }
+
+    cartRemoveItem(itemId) {
+        utils.api.cart.itemRemove(itemId, (err, response) => {
+            if (response.data.status === 'succeed') {
+                this.refreshContent();
+            } else {
+                alert(response.data.errors.join('\n'));
+            }
+        });
+    }
+
+    refreshContent() {
+        utils.api.cart.getContent('cart/content', (err, response) => {
+            console.log(response);
+            $('[data-cart-content]').html(response.content);
+            this.bindEvents();
+        });
+    }
+
+    bindEvents() {
         // cart update
-        $('body').on('click', '.cart-update', (event) => {
+        $('.cart-update').on('click', (event) => {
             let $target = $(event.currentTarget);
 
             // prevent form submission
             event.preventDefault();
 
             // update cart quantity
-            cartUpdate($target);
+            _.bind(_.debounce(this.cartUpdate, this.debounceTimeout), this)($target);
         });
 
-        $('body').on('click', '.cart-remove', (event) => {
+        $('.cart-remove').on('click', (event) => {
             let itemId = $(event.currentTarget).data('cart-itemid');
 
             // prevent form submission
             event.preventDefault();
 
             // remove item from cart
-            cartRemoveItem(itemId);
+            this.cartRemoveItem(itemId);
         });
 
-        next();
-    }
+        $('.shipping-estimate-submit').on('click', (event) => {
+            let params = {
+                country_id: $('.shipping-estimate [name="country"]').val(),
+                state_id: $('.shipping-estimate [name="state"]').val(),
+                zip_code: $('.shipping-estimate [name="zip"]').val()
+            };
 
-    refreshContent() {
-        let options = {
-            template: 'cart/content'
-        };
+            event.preventDefault();
 
-        utils.api.cart.getContent(options, (err, response) => {
-            $('[data-cart-content]').html(response.content);
+            utils.api.cart.getShippingQuotes(params, 'cart/shipping-quotes', (err, response) => {
+                $('.shipping-quotes').html(response.content);
+            });
         });
+
+        $('body').on('click', 'select-shipping-quote', (event) => {
+            var quoteId = $('.shipping-quote:checked').val();
+            event.preventDefault();
+            console.log(quoteId);
+            utils.api.cart.submitShippingQuote(quoteId, (response) => {
+                this.refreshContent();
+            });
+        });
+
+        $('.shipping-estimate-show').on('click', (event) => {
+            $(event.currentTarget).hide();
+            $('.estimator-wrapper').show();
+        });
+
+
+        $('.shipping-estimate-hide').on('click', (event) => {
+            $('.estimator-wrapper').hide();
+            $('.shipping-estimate-show').show();
+        });
+
+
+        $('.estimator-wrapper').hide();
     }
 }

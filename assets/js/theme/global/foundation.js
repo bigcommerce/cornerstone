7b712bc9f6dc2c3cc2de00bf50dc8f8131e5a0d2
haubc <hau.nguyen@bigcommerce.com>
2015-06-10 10:17:28 -0700

    BIG-16046 Remove custom css for modal background; Rename navigation.js to foundation.js; Add language strings

--- assets/js/theme/global/foundation.js
+++ assets/js/theme/global/foundation.js
@@ -1,12 +1,16 @@
-import 'jquery';
+import $ from 'jquery';
 import 'foundation/js/foundation/foundation';
 import 'foundation/js/foundation/foundation.dropdown';
+import 'foundation/js/foundation/foundation.reveal';
 
 export default function() {
     $(document).foundation({
         dropdown: {
             // specify the class used for active dropdowns
             active_class: 'is-open'
+        },
+        reveal: {
+            bg_class: 'modal-background'
         }
     });
 }

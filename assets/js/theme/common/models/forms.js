e19c44e2bc91ec057e48a71f29fdaf7336177949
Mick Ryan <mick@evilbits.io>
2015-07-08 13:18:05 -0700

    BIG-19721 - JS Errors on login page

---
+++ assets/js/theme/common/models/forms.js
@@ -0,0 +1,15 @@
+export default {
+    email(value) {
+        var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+        return re.test(value);
+    },
+
+    /**
+     * Validates a password field
+     * @param value
+     * @returns {boolean}
+     */
+        password(value) {
+        return value.length > 0;
+    }
+}

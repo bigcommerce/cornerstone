f477249241098d86f4696ca179c2e57c6b753b50
Mick Ryan <mick@evilbits.io>
2015-07-14 14:52:37 -0700

    BIG-19732 * We were not using Foundations dropdown in the proper manner so I re-implemented it for our simpler use case. * Changed data-tags and removed foundation data-tags * Wrote supporting JS for click handlers, and keeping in mind for screen readers.

--- assets/js/theme/global/quick-search.js
+++ assets/js/theme/global/quick-search.js
@@ -2,15 +2,40 @@ import $ from 'jquery';
 import _ from 'lodash';
 import utils from 'bigcommerce/stencil-utils';
 
+function hideSearch($searchDiv) {
+    $searchDiv.removeClass('is-open f-open-dropdown').attr('style', '0px').attr('aria-hidden', 'true');
+}
+
+function showSearch($searchDiv) {
+    $searchDiv.addClass('is-open f-open-dropdown').attr('style', 'top: 49px;').attr('aria-hidden', 'false');
+    event.stopPropagation();
+}
+
 export default function () {
     let $quickSearchResults = $('.quickSearchResults');
+    let $quickSearchDiv = $('#quickSearch');
 
-    //stagger searching for 400ms after last input
+    $('[data-search="quickSearch"]').on('click', (event) => {
+        $quickSearchDiv.hasClass('is-open') ? hideSearch($quickSearchDiv) : showSearch($quickSearchDiv);
+    });
+
+    $quickSearchDiv.click((event) => {
+        event.stopPropagation();
+    });
+
+    $('body').click(function () {
+        hideSearch($quickSearchDiv);
+    });
+
+    //stagger searching for 200ms after last input
     let doSearch = _.debounce((searchQuery) => {
         utils.api.search.search(searchQuery, {template: 'search/quick-results'}, (err, response) => {
+            if (err) {
+                throw new Error(err);
+            }
             $quickSearchResults.html(response);
         });
-    }, 400);
+    }, 200);
 
     utils.hooks.on('search-quick', (event) => {
         let searchQuery = $(event.currentTarget).val();

6019c91e0a291685c4c5591ba1cf27f7bd4a903a
David Chin <davidchin.yf@gmail.com>
2015-07-23 20:19:24 +1000

    BIG-20064: Fix quick view cart showing off-screen

--- assets/js/theme/global/cart-preview.js
+++ assets/js/theme/global/cart-preview.js
@@ -4,18 +4,26 @@ import 'foundation/js/foundation/foundation.dropdown';
 import utils from 'bigcommerce/stencil-utils';
 
 export default function () {
-    let $cart = $('[data-cart-preview]'),
-        $cartDropdown = $('#cart-preview-dropdown');
+    let loadingClass = 'is-loading',
+        $cart = $('[data-cart-preview]'),
+        $cartDropdown = $('#cart-preview-dropdown'),
+        $cartLoading = $('<div class="loadingOverlay"></div>');
 
     $cart.on('click', (event) => {
         let options = {
             template: 'common/cart-preview'
         };
 
+        $cartDropdown
+            .addClass(loadingClass)
+            .html($cartLoading);
+
         event.preventDefault();
 
         utils.api.cart.getContent(options, (err, response) => {
-            $cartDropdown.html(response.content);
+            $cartDropdown
+                .removeClass(loadingClass)
+                .html(response.content);
         });
     });
 }

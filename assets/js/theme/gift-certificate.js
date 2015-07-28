541b19a8db3bb2deee0cbd25384f7607afe74896
Mick Ryan <mick@evilbits.io>
2015-07-28 14:08:51 -0700

    Bump Nod to 2.0.10
    * Removed hardcoding in gift-certs
    * Changed config.js and package.json to use 2.0.10 version on Nod

--- assets/js/theme/gift-certificate.js
+++ assets/js/theme/gift-certificate.js
@@ -1,5 +1,5 @@
 import PageManager from '../page-manager';
-import nod from 'github:casperin/nod@2.0.4/nod';
+import nod from 'casperin/nod';
 export default class GiftCertificate extends PageManager {
     constructor() {
         super();

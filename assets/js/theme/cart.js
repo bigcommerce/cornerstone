dc0d598b04e6d76809048af1afa61e1269da93d4
Mario Campa <mario.campa@bigcommerce.com>
2015-08-03 10:23:20 -0700

    Remove link confirmation

--- assets/js/theme/cart.js
+++ assets/js/theme/cart.js
@@ -197,7 +197,7 @@ export default class Cart extends PageManager {
 
                 this.bindGiftWrappingForm();
             });
-        });
+        });        
     }
 
     bindGiftWrappingForm() {

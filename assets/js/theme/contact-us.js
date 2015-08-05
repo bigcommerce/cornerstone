2c9128de537af032b28188a1ec3f4df91a21ae73
Chris Hegre <christopher.hegre@bigcommerce.com>
2015-08-05 15:09:56 -0700

    Validate contact form before submission

--- assets/js/theme/contact-us.js
+++ assets/js/theme/contact-us.js
@@ -1,5 +1,6 @@
 import PageManager from '../page-manager';
 import nod from './common/nod';
+import $ from 'jquery';
 import forms from './common/models/forms';
 
 export default class ContactUs extends PageManager {
@@ -8,32 +9,44 @@ export default class ContactUs extends PageManager {
     }
 
     loaded() {
-        this.contactUsModel = forms;
         this.registerContactFormValidation();
     }
 
     registerContactFormValidation() {
-        let contactUsValidator = nod({
-            submit: '#contactus-form input[type="submit"]'
-        });
+
+        let formSelector = 'form[data-contact-form]',
+            contactUsValidator = nod({
+                submit: `${formSelector} input[type="submit"]`
+            }),
+            $contactForm = $(formSelector);
 
         contactUsValidator.add([
             {
-                selector: '#contactus-form input[name="contact_email"]',
+                selector: `${formSelector} input[name="contact_email"]`,
                 validate: (cb, val) => {
-                    let result = this.contactUsModel.email(val);
+                    let result = forms.email(val);
                     cb(result);
                 },
                 errorMessage: "You must enter an email address"
             },
             {
-                selector: '#contactus-form textarea[name="contact_question"]',
+                selector: `${formSelector} textarea[name="contact_question"]`,
                 validate: (cb, val) => {
-                    let result = this.contactUsModel.notEmpty(val);
+                    let result = forms.notEmpty(val);
                     cb(result);
                 },
                 errorMessage: "You must enter your question"
             }
         ]);
+
+        $contactForm.submit((event) => {
+            contactUsValidator.performCheck();
+
+            if (contactUsValidator.areAll('valid')) {
+                return;
+            }
+
+            event.preventDefault();
+        });
     }
 }

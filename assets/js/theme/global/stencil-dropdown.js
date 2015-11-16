44b3da249a591403c96d67bf8aa110a2f16a63e7
haubc <hau.nguyen@bigcommerce.com>
2015-11-16 14:37:21 -0800

    BIG-22181 Fix currency selector and seperate concerns in stencil-dropdown.js

--- assets/js/theme/global/stencil-dropdown.js
+++ assets/js/theme/global/stencil-dropdown.js
@@ -1,22 +1,40 @@
 import $ from 'jquery';
 
-export default {
+export default class StencilDropdown {
+    constructor(extendables) {
+        this.extendables = extendables;
+    }
+
+    /**
+     * @param $dropDown
+     * @param style
+     */
     hide($dropDown, style) {
         if (style) {
             $dropDown.attr('style', style);
         }
-        $('#search_query').blur();
+
+        // callback "hide"
+        if (this.extendables && this.extendables.hide) {
+            this.extendables.hide(event);
+        }
+
         $dropDown.removeClass('is-open f-open-dropdown').attr('aria-hidden', 'true');
-    },
+    }
+
     show($dropDown, event, style) {
         if (style) {
             $dropDown.attr('style', style).attr('aria-hidden', 'false');
         }
 
         $dropDown.addClass('is-open f-open-dropdown').attr('aria-hidden', 'false');
-        $('#search_query').focus();
-        event.stopPropagation();
-    },
+
+        // callback "show"
+        if (this.extendables && this.extendables.show) {
+            this.extendables.show(event);
+        }
+    }
+
     bind($dropDownTrigger, $container, style) {
         let modalOpened = false;
 
@@ -27,15 +45,13 @@ export default {
                 $cart.click();
             }
 
-            $container.hasClass('is-open') ? this.hide($container) : this.show($container, event, style);
+            $container.hasClass('is-open') ? this.hide($container, event) : this.show($container, event, style);
         });
 
         $('body').click((e) => {
-            // If the target element has this data tag or one of it's parents, do not close the search results
-            // We have to specify `.modal-background` because of limitations around Foundation Reveal not allowing
-            // any modification to the background element.
-            if ($(e.target).closest('[data-prevent-quick-search-close], .modal-background').length === 0) {
-                this.hide($container);
+            // Call onClick handler
+            if (this.extendables && this.extendables.onBodyClick) {
+                this.extendables.onBodyClick(e, $container);
             }
         }).on('keyup', (e) => {
             // If they hit escape and the modal isn't open, close the search
@@ -47,5 +63,5 @@ export default {
         }).on('close.fndtn.reveal', '[data-reveal]', () => {
             modalOpened = false;
         });
-    },
-};
+    }
+}

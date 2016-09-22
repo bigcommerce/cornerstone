da7577d9f395e8e285f98971cc7b6142b044bd05
Mario Campa <mcampa@gmail.com>
2016-09-22 13:29:26 -0700

    MERC-1175 fix some styling issues

--- assets/js/theme/common/select-option-plugin.js
+++ assets/js/theme/common/select-option-plugin.js
@@ -10,10 +10,10 @@ import $ from 'jquery';
  * (eg. if you have option 1, 2, 3 and 2 is hidden, when you select 3,
  * serializeArray() will use the value of 2 instead of 3)
  */
-$.fn.toggleOption = function (show) {
-    var currentSelectElement = $(this).closest('select'), // the select containing this
-        disabledSelectElement, // the disabled select element
-        selectElement; // the real select element
+function toggleOption(show) {
+    const currentSelectElement = $(this).closest('select'); // the select containing this
+    let disabledSelectElement; // the disabled select element
+    let selectElement; // the real select element
 
     if (currentSelectElement.is(':disabled')) {
         disabledSelectElement = currentSelectElement;
@@ -23,23 +23,23 @@ $.fn.toggleOption = function (show) {
         disabledSelectElement = currentSelectElement.data('linkedSelectElement');
         if (!disabledSelectElement) {
             // create the disabled placeholder select element
-            disabledSelectElement =
-                $('<select>')
-                    .prop('disabled', true)
-                    .hide()
-                    .attr('name', currentSelectElement.attr('name'))
-                    .addClass(currentSelectElement.attr('class'))
-                    .data('linkedSelectElement', selectElement)
-                    .insertAfter(selectElement);
+            disabledSelectElement = $('<select>')
+                .prop('disabled', true)
+                .hide()
+                .attr('name', currentSelectElement.attr('name'))
+                .addClass(currentSelectElement.attr('class'))
+                .data('linkedSelectElement', selectElement)
+                .insertAfter(selectElement);
+
             selectElement.data('linkedSelectElement', disabledSelectElement);
         }
     }
 
     // move the option to the correct select element if required
     if (currentSelectElement.is(':disabled') && show) {
-        var previousIndex = this.data('index');
+        const previousIndex = this.data('index');
         if (previousIndex > 0) {
-            this.insertAfter(selectElement.find('option:eq(' + (previousIndex-1) + ')'));
+            this.insertAfter(selectElement.find(`option:eq(${previousIndex - 1})`));
         } else {
             $(this).prependTo(selectElement);
         }
@@ -47,4 +47,6 @@ $.fn.toggleOption = function (show) {
         this.data('index', currentSelectElement.find('option').index(this));
         $(this).prependTo(disabledSelectElement);
     }
-};
+}
+
+$.fn.toggleOption = toggleOption;

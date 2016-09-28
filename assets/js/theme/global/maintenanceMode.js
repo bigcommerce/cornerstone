73d22acab8854c9789ecb0851022ad86c2014b2b
David Chin <davidchin.yf@gmail.com>
2016-09-28 12:08:45 +1000

    Use CSS to position maintenance banner

--- assets/js/theme/global/maintenanceMode.js
+++ assets/js/theme/global/maintenanceMode.js
@@ -8,8 +8,6 @@ export default function (maintenanceMode = {}) {
     const header = maintenanceMode.header;
     const notice = maintenanceMode.notice;
 
-    let scrollTop = 0;
-
     if (!(header && notice)) {
         return;
     }
@@ -22,22 +20,4 @@ export default function (maintenanceMode = {}) {
     $element.html(`<p class="maintenanceNotice-header">${header}</p>${notice}`);
 
     $('body').append($element);
-
-    $(window)
-        .bind('scroll', () => {
-            $element.css('top', `${($('body').scrollTop() + scrollTop)}px`);
-        })
-        .bind('resize', () => {
-            const menuWidth = $('#maintenance-notice').width();
-
-            if (menuWidth + $('#maintenance-notice').offset().left > $(window).width()) {
-                const newLeft = (`${$(window).width() - menuWidth - 50}px`);
-
-                $('#maintenance-notice').css('left', newLeft);
-            }
-        });
-
-    scrollTop = $('#maintenance-notice').scrollTop() - $('body').scrollTop();
-
-    $(window).trigger('resize');
 }

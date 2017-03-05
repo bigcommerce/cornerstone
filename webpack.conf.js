b2b9007fa094608c9afa3dc364f69b28a080d8aa
Michael Schock <michael.schock@bigcommerce.com>
2017-03-05 01:38:42 -0800

    webpack 2

--- webpack.conf.js
+++ webpack.conf.js
@@ -1,36 +1,73 @@
-var webpack = require('webpack');
+var CleanWebpackPlugin = require('clean-webpack-plugin'),
+    config = require('./config.json'),
+    LodashModuleReplacementPlugin = require('lodash-webpack-plugin'),
+    path = require('path'),
+    webpack = require('webpack');
 
 module.exports = {
-    watch: false,
-    devtool: 'source-map',
-    context: __dirname,
-    entry: {
-        'theme-bundle': './assets/js/app.js',
-    },
-    output: {
-        path: `./assets/dist`,
-        filename: '[name].js',
-    },
     bail: true,
+    context: __dirname,
+    devtool: 'source-map',
+    entry: {
+        main: './assets/js/app.js',
+    },
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.js$/,
-                loader: 'babel',
                 include: /(assets\/js|assets\\js|stencil-utils)/,
-                query: {
-                    compact: false,
-                    cacheDirectory: true,
-                    presets: ['es2015-loose'],
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                        compact: true,
+                        minified: true,
+                        plugins: [
+                            'dynamic-import-webpack', // Needed for dynamic imports.
+                            'lodash', // Automagically tree-shakes lodash.
+                            'transform-regenerator', // Transforms async and generator functions.
+                        ],
+                        presets: [
+                            ['env', {
+                                loose: true, // Enable "loose" transformations for any plugins in this preset that allow them.
+                                modules: false, // Don't transform modules; needed for tree-shaking.
+                                useBuiltIns: true, // Tree-shake babel-polyfill.
+                            }],
+                        ],
+                    }
                 }
             }
-        ]
+        ],
+    },
+    output: {
+        chunkFilename: 'theme-bundle.chunk.[name].js',
+        filename: 'theme-bundle.[name].js',
+        path: path.resolve(__dirname, "assets/dist"),
     },
     plugins: [
+        new CleanWebpackPlugin(['assets/dist'], {
+            verbose: true,
+            watch: false,
+        }),
+        new webpack.LoaderOptionsPlugin({
+             minimize: true,
+        }),
+        new LodashModuleReplacementPlugin, // Complements 'babel-plugin-lodash by shrinking it's cherry-picked builds further.
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
             'window.jQuery': 'jquery',
-        })
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            children: true,
+            minChunks: 2,
+        }),
     ],
+    resolve: {
+        alias: {
+            jquery: path.resolve(__dirname, 'node_modules/jquery/dist/jquery.min.js'),
+            jstree: path.resolve(__dirname, 'node_modules/jstree/dist/jstree.min.js'),
+        },
+    },
+    watch: false,
 };

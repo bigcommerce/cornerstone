e58a712f5e4c0d129c8fc77455acbc2e69e91601
Matt Olson <matt.olson@bigcommerce.com>
2018-03-10 15:43:00 -0800

    STRF-4603: Webpack 4 + Javascript optimizations
    * Upgrade to webpack 4
    * Clean up stencil bootstrap
    * Simplify interface for PageManager and get rid of async library dependency
    * Get rid of html5-history-api library dependency (no longer needed)
    * Get rid of fastclick library dependency (no longer needed)
    * Upgrade several babel dependencies
    * Separate webpack config into common, dev, prod, and add npm scripts to build
    * Get rid of minifications in babel loader, instead rely on webpack
    * Get rid of LoaderOptionsPlugin
    * Get rid of CommonsChunkPlugin (webpack 4 will automaticaly do this for prod)

---
+++ webpack.dev.js
@@ -0,0 +1,13 @@
+const webpack = require('webpack'),
+      merge = require('webpack-merge'),
+      commonConfig = require('./webpack.common.js');
+
+module.exports = merge(commonConfig, {
+    mode: 'development',
+    devtool: 'source-map',
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': 'development',
+        }),
+    ],
+});
---
+++ webpack.prod.js
@@ -0,0 +1,12 @@
+const webpack = require('webpack'),
+      merge = require('webpack-merge'),
+      commonConfig = require('./webpack.common.js');
+
+module.exports = merge(commonConfig, {
+    mode: 'production',
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': 'production',
+        }),
+    ],
+});

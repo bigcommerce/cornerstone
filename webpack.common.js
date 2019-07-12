789a3b717745f538ab87110ddd0efeed21e9103f
Nathan Booker <nathan.booker@bigcommerce.com>
2019-07-12 14:48:03 -0500

    Remove nanobar
    This loading bar just makes the site seem slow, removing it.

--- webpack.common.js
+++ webpack.common.js
@@ -67,7 +67,6 @@ module.exports = {
             jquery: path.resolve(__dirname, 'node_modules/jquery/dist/jquery.min.js'),
             jstree: path.resolve(__dirname, 'node_modules/jstree/dist/jstree.min.js'),
             lazysizes: path.resolve(__dirname, 'node_modules/lazysizes/lazysizes.min.js'),
-            nanobar: path.resolve(__dirname, 'node_modules/nanobar/nanobar.min.js'),
             'slick-carousel': path.resolve(__dirname, 'node_modules/slick-carousel/slick/slick.min.js'),
             'svg-injector': path.resolve(__dirname, 'node_modules/svg-injector/dist/svg-injector.min.js'),
             sweetalert2: path.resolve(__dirname, 'node_modules/sweetalert2/dist/sweetalert2.min.js'),

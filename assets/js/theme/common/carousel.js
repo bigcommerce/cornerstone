d674d82ddbbb7030136935d765b2b8055033be1a
Yurii Zusik <yurii.zusik@bigcommerce.com>
2020-07-03 10:33:25 +0300

    fix(Cornnerstone): BCTHEME-52 stretch & no strech carousel images option works

--- assets/js/theme/common/carousel.js
+++ assets/js/theme/common/carousel.js
@@ -10,12 +10,7 @@ export default function () {
     // Alternative image styling for IE, which doesn't support objectfit
     if (typeof document.documentElement.style.objectFit === 'undefined') {
         $('.heroCarousel-slide').each((index, element) => {
-            const $container = $(element);
-            const imgUrl = $container.find('img').data('lazy');
-
-            if (imgUrl) {
-                $container.css('backgroundImage', `url(${imgUrl})`).addClass('compat-object-fit');
-            }
+            $(element).addClass('compat-object-fit');
         });
     }
 }

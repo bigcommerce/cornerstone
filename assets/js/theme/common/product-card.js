83c0f7d9ca38b0cd9d51379082fcd6b9f0b3883d
Yurii Zusik <yurii.zusik@bigcommerce.com>
2020-07-23 14:48:02 +0300

    feature(themes): BCTHEME-71 Add To Cart & Quick View buttons now appear when tabbing on product cards (remote linter fixes)

--- assets/js/theme/common/product-card.js
+++ assets/js/theme/common/product-card.js
@@ -7,24 +7,24 @@ const previousCard = {
 const onCardFocus = (event) => {
     const $element = $(event.target);
 
-    const $cardContainer = $element.parents('.card'); 
+    const $cardContainer = $element.parents('.card');
 
     const isFocusInCard = $cardContainer.length;
 
     const { $previous } = previousCard;
-    
+
     if (isFocusInCard) {
-        $previous && $previous.removeClass(focusedCardClass); 
+        if ($previous) $previous.removeClass(focusedCardClass);
         $cardContainer.addClass(focusedCardClass);
         previousCard.$previous = $cardContainer;
         return;
     }
 
-    $previous && $previous.removeClass(focusedCardClass);
+    if ($previous) $previous.removeClass(focusedCardClass);
 };
 
 export default function () {
-    const $allFocusableElements = $(focusableElementsSelector); 
+    const $allFocusableElements = $(focusableElementsSelector);
 
     $allFocusableElements.on('focus', onCardFocus);
-};
+}

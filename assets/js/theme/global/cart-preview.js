8b5f8d0ba26d75d3f120906a2691e9b36be28dda
Tymur Biedukhin <tymur.biedukhin@bigcommerce.com>
2020-08-26 17:21:52 +0300

    Eslint fix

--- assets/js/theme/global/cart-preview.js
+++ assets/js/theme/global/cart-preview.js
@@ -17,13 +17,13 @@ export default function (secureBaseUrl, cartId) {
 
     $body.on('cart-quantity-update', (event, quantity) => {
         $cart.attr('aria-label', (_, prevValue) => prevValue.replace(/\d+/, quantity));
-      
+
         if (!quantity) {
             $cart.addClass('navUser-item--cart__hidden-s');
         } else {
             $cart.removeClass('navUser-item--cart__hidden-s');
         }
-      
+
         $('.cart-quantity')
             .text(quantity)
             .toggleClass('countPill--positive', quantity > 0);

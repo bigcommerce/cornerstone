0be2ea600e6e040c184b4a284587645984f3ded4
Alexander Guyon <guyonalemsu@gmail.com>
2021-03-07 22:24:07 -0500

    implement some feature requests; see README.md

--- assets/js/theme/category.js
+++ assets/js/theme/category.js
@@ -30,7 +30,19 @@ export default class Category extends CatalogPage {
     onReady() {
         this.arrangeFocusOnSortBy();
 
+        //determine wheter or not the 'Remove All Items' button should be visible
+        this.showRemoveAllItems();
+
         $('[data-button-type="add-cart"]').on('click', (e) => this.setLiveRegionAttributes($(e.currentTarget).next(), 'status', 'polite'));
+
+        // functionality associated with the 'Add All to Cart' button
+        this.addAllToCartHandler();
+
+        // functionality associated with the 'Remove All Items' button
+        this.removeItemsFromCartHandler();
+
+        // functionality associated with hovering over a card-img-container
+        this.cardImageHandler();
 
         this.makeShopByPriceFilterAccessible();
 
@@ -47,6 +59,124 @@ export default class Category extends CatalogPage {
 
         this.ariaNotifyNoProducts();
     }
+
+    showRemoveAllItems(){
+        fetch(`/api/storefront/carts`,{
+            method: "GET",
+            credentials: "same-origin"
+        })
+        .then(response => response.json())
+        .then(carts => carts[0])
+        .then(cart => {
+            if (!cart){
+                //cart is not defined
+                $('#remove-all').hide()
+            }
+        })
+    }
+
+    addAllToCartHandler(){
+        $('.category#add-all-from-category').on('click', (e) => {
+            e.preventDefault()
+            const products = this.context.categoryProducts;
+            if (products.length > 0 ){
+                fetch(`/api/storefront/carts`,{
+                    method: "GET",
+                    credentials: "same-origin"
+                })
+                .then(response => response.json())
+                .then(carts => carts[0])
+                .then(cart => {
+                    if (cart && cart.id){
+                        // 'existant cart'
+                        fetch(`/api/storefront/carts/${cart.id}/items`,{
+                            method: "POST",
+                            credentials: "same-origin",
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                            body: JSON.stringify({
+                                lineItems: products.map(x => ({quantity:1, productId: x.id}))
+                            })
+                        })
+                    } else {
+                        // 'non-existant cart'
+                        // for whatever reason, this does not work *sometimes* 
+                        //(as in the POST request/response(returns a new cart) is succesful, the cart is not created server-side?)
+                        fetch(`/api/storefront/carts`, {
+                            method: "POST",
+                            credentials: "same-origin",
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                            body: JSON.stringify({
+                                lineItems: products.map(x => ({quantity:1, productId: x.id}))
+                            })
+                        })
+                        .then(response => response.json())
+                        .then(response => console.log(response));
+                    }
+                })
+
+                //  After mutating the state of the cart, reveal the 'Remove All Items' button
+                $('#remove-all').show()
+            }
+        })
+    }
+
+    removeItemsFromCartHandler(){
+        $('.category#remove-all').on('click', (e) => {
+            e.preventDefault()
+            fetch(`/api/storefront/carts`,{
+                method: "GET",
+                credentials: "same-origin"
+            })
+            .then(response => response.json())
+            .then(carts => carts[0])
+            .then(cart => {
+                if (cart && cart.id){
+                    //https://developer.bigcommerce.com/api-reference/storefront/carts/cart-items/deletecartlineitem
+                    // 'Removing the last line_item in the Cart deletes the Cart.'
+                    fetch(`/api/storefront/carts/${cart.id}`,{
+                        method: "DELETE",
+                        credentials: "same-origin"
+                    })
+                }
+            })
+        })
+    }
+
+    cardImageHandler(){
+        let originalSrcSet;
+        let originalSrc;
+        $('.card-image').hover(
+            //on hover
+            (e) => {
+                const currentTarget = $(e.currentTarget);
+                if(currentTarget.data('hover-srcset')) {
+                    originalSrcSet = currentTarget.data('srcset');
+                    currentTarget.attr('data-srcset', currentTarget.data('hover-srcset'));
+                    currentTarget.attr('srcset', currentTarget.data('hover-srcset'));
+                    
+                    originalSrc = currentTarget.attr('src');
+                    currentTarget.attr('src', currentTarget.data('hover-src'));
+
+                    console.log('on hover', currentTarget);
+                }
+            },
+            //off hover
+            (e) => {
+                const currentTarget = $(e.currentTarget)
+                if(currentTarget.data('hover-srcset')){
+                    currentTarget.attr('data-srcset',originalSrcSet);
+                    currentTarget.attr('srcset',originalSrcSet);
+                    currentTarget.attr('src', originalSrc);
+                    console.log('off hover', currentTarget);
+                }
+            }
+
+        )
+    };
 
     ariaNotifyNoProducts() {
         const $noProductsMessage = $('[data-no-products-notification]');

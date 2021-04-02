f5f802652f274843e0013368821fbe2f841b0123
Yurii Zusik <yurii.zusik@bigcommerce.com>
2021-04-03 01:30:51 +0300

    feat(storefront): BCTHEME-425 Incorrect focus order for product carousels

---
+++ assets/js/theme/common/carousel/constants.js
@@ -0,0 +1,1 @@
+export const FOCUSABLE_ELEMENTS_SELECTOR = '[href], button, input, textarea, select, details, [contenteditable="true"], [tabindex]';
--- assets/js/theme/common/carousel/utils/tooltipSetup.js
+++ assets/js/theme/common/carousel/utils/tooltipSetup.js
@@ -2,30 +2,12 @@ const TOOLTIP_DATA_SELECTOR = 'data-carousel-tooltip';
 const TOOLTIP_CLASS = 'carousel-tooltip';
 const TOOLTIP_NODE = `<span ${TOOLTIP_DATA_SELECTOR} class="${TOOLTIP_CLASS}"></span>`;
 
-const setupTooltipAriaLabel = ($node) => {
+export default ($node) => {
     const $existedTooltip = $node.find(`[${TOOLTIP_DATA_SELECTOR}]`);
     if ($existedTooltip.length) {
         $existedTooltip.attr('aria-label', $node.attr('aria-label'));
     } else {
         const $tooltip = $(TOOLTIP_NODE).attr('aria-label', $node.attr('aria-label'));
         $node.append($tooltip);
-    }
-};
-
-const setupArrowTooltips = (...arrowNodes) => {
-    arrowNodes.forEach($arrow => setupTooltipAriaLabel($arrow));
-};
-
-const setupDotTooltips = ($dots) => {
-    $dots.children().each((idx, dot) => setupTooltipAriaLabel($('[data-carousel-dot]', dot)));
-};
-
-export default ($prevArrow, $nextArrow, $dots) => {
-    if ($prevArrow && $nextArrow) {
-        setupArrowTooltips($prevArrow, $nextArrow);
-    }
-
-    if ($dots) {
-        setupDotTooltips($dots);
     }
 };

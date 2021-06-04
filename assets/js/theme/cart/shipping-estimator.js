ebf111226d0dc94b91435b6a77b6a57c9d81a76c
Yurii Zusik <yurii.zusik@bigcommerce.com>
2021-06-04 11:49:00 +0300

    Fix eslint errors in cart.js, shipping-estimator.js and test in cart.spec.js

--- assets/js/theme/cart/shipping-estimator.js
+++ assets/js/theme/cart/shipping-estimator.js
@@ -64,7 +64,7 @@ export default class ShippingEstimator {
 
                     cb(result);
                 },
-                errorMessage: this.shippingErrorMessages.country
+                errorMessage: this.shippingErrorMessages.country,
             },
         ]);
     }
@@ -86,7 +86,7 @@ export default class ShippingEstimator {
 
                     cb(result);
                 },
-                errorMessage: this.shippingErrorMessages.province
+                errorMessage: this.shippingErrorMessages.province,
             },
         ]);
     }

fb6a749be58cdfc5257dbd3c855a6e10dec45cfc
Saurabh Gedam <saurabh.gedam>
2021-09-10 00:58:36 -0700

    fix(search): ES-2439 adding a variable

--- assets/js/theme/global/quick-search.js
+++ assets/js/theme/global/quick-search.js
@@ -33,6 +33,7 @@ export default function () {
     };
 
     // stagger searching for 200ms after last input
+    const debounceWaitTime = 1200;
     const doSearch = _.debounce((searchQuery) => {
         utils.api.search.search(searchQuery, { template: 'search/quick-results' }, (err, response) => {
             if (err) {
@@ -62,7 +63,7 @@ export default function () {
                 }, 100);
             }
         });
-    }, 1200);
+    }, debounceWaitTime);
 
     utils.hooks.on('search-quick', (event, currentTarget) => {
         const searchQuery = $(currentTarget).val();

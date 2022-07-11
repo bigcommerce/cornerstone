5900d38f6de884567c14984685111a6acc8b8270
Samuel Reid <samjohnreid@gmail.com>
2022-07-11 11:13:35 -0400

    trying to control existing youtube iframe vid with external element

--- assets/js/theme/page.js
+++ assets/js/theme/page.js
@@ -3,14 +3,30 @@ import PageManager from './page-manager';
 export default class Page extends PageManager {
     constructor(context) {
         super(context);
-        this.url = window.location.pathname;
+        this.pathname = window.location.pathname;
     }
 
+    addVideoClickLayer() {
+        const videoContainer = document.getElementsByClassName('video-container-realmax');
+        const videoClickLayer = document.createElement('div');
+        videoClickLayer.classList.add('video-click-layer');
+
+        videoContainer[0].appendChild(videoClickLayer);
+
+        const player = document.querySelector('[data-src="https://www.youtube.com/embed/bfctwig1HUQ?start=0&end=0&autoplay=1&loop=1&mute=1&playlist=bfctwig1HUQ&version=3&rel=0"]');
+        console.log('player......? ', player);
+
+        videoClickLayer.addEventListener('click', (el) => {
+            // 1. unmute the video
+
+            // 2. play the video from the beginning
+            
+            // 3. remove the clicklayer
+            el.target.style.display = 'none';
+        });
+    }
+    
     onReady() {
-        if (this.url === '/') {
-            console.log('homepage!');
-        } else {
-            console.log('NOT homepage!');
-        }
+        this.pathname === '/' && this.addVideoClickLayer();
     }
 }

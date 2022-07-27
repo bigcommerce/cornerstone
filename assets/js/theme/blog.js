3c21cb16911c9313a132bfbada5889d1d320727d
Samuel Reid <samjohnreid@gmail.com>
2022-07-27 17:48:10 -0400

    finishing up getting all the data in from Shogun as we need it

--- assets/js/theme/blog.js
+++ assets/js/theme/blog.js
@@ -1,20 +1,24 @@
 import PageManager from './page-manager';
 import { api } from '@bigcommerce/stencil-utils';
 
+const extractText = (strToParse, strStart, strFinish) => {
+    return strToParse.match(strStart + "(.*?)" + strFinish)[1];
+};
+
+const options = {
+    template: 'blog/blog-json',
+    config: {
+        blog: {
+            posts: {
+                limit: 20,
+                summary: 500
+            },
+        },
+    },
+};
+
 export default class Blog extends PageManager {
     getBlogPosts() {
-        const options = {
-            template: 'blog/blog-json',
-            config: {
-                blog: {
-                    posts: {
-                        limit: 20,
-                        summary: 500
-                    },
-                },
-            },
-        };
-
         api.getPage('/blog/', options, (error, response) => {
             if (error) return console.error(error);
             const postsJson = JSON.parse(response);
@@ -22,7 +26,21 @@ export default class Blog extends PageManager {
         });
     }
 
+    extractSummaryText() {
+        api.getPage('/blog/', options, (error, response) => {
+            if (error) return console.error(error);
+            const postsJson = JSON.parse(response);
+            
+            const summaryTextContainers = document.querySelectorAll('.summary-text');
+            summaryTextContainers.forEach((summary) => {
+                const summaryText = summary.innerText;
+                summary.textContent = extractText(summaryText, 'BLOG-SUMMARY-TEXT-BEGIN', 'BLOG-SUMMARY-TEXT-END');
+            });
+        });
+    }
+
     onReady() {
         this.getBlogPosts();
+        this.extractSummaryText();
     }
 }

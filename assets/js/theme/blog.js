3ada50b326168f3609fc54854e434d73caf07192
Samuel Reid <samjohnreid@gmail.com>
2022-07-28 14:37:53 -0400

    adding feature to extract blog tag

--- assets/js/theme/blog.js
+++ assets/js/theme/blog.js
@@ -1,9 +1,7 @@
 import PageManager from './page-manager';
 import { api } from '@bigcommerce/stencil-utils';
 
-const extractText = (strToParse) => {
-    return strToParse.match('BLOG-SUMMARY-TEXT-BEGIN' + '(.*?)' + 'BLOG-SUMMARY-TEXT-END')[1];
-};
+// ----------------------------------------------------------------------------------------------------
 
 const options = {
     template: 'blog/blog-json',
@@ -17,29 +15,65 @@ const options = {
     },
 };
 
-export default class Blog extends PageManager {
-    buildFeatured3() {
-        api.getPage('/blog/', options, (error, response) => {
-            if (error) return console.error(error);
-            
-            const postsJson = JSON.parse(response);
-            const featuredPost = postsJson.posts.find(post => post.tags.some(tag => tag.name === 'featured'));
-            const container = document.getElementById('featured-3');
+const extractSummary = (initialSummary) => {
+    return initialSummary.match('BLOG-SUMMARY-TEXT-BEGIN' + '(.*?)' + 'BLOG-SUMMARY-TEXT-END')[1];
+};
 
+const extractTag = (tags) => {
+    var tagName;
+    tags.forEach((tag) => {
+        console.log('tag.name', tag.name);
+        switch(tag.name) {
+            case 'industry trend':
+                tagName = 'Industry Trend';
+                break;
+            case 'installation':
+                tagName = 'Installation';
+                break;
+            case 'news':
+                tagName = 'News';
+                break;
+            case 'press release':
+                tagName = 'Press Release';
+                break;
+            case 'service':
+                tagName = 'Service';
+                break;
+            default:
+                '';
+        }
+    });
+    return tagName;
+};
+
+// ----------------------------------------------------------------------------------------------------
+
+const buildFeatured3 = () => {
+    api.getPage('/blog/', options, (error, response) => {
+        if (error) return console.error(error);
+        
+        const postsJson = JSON.parse(response);
+        const featuredPost = postsJson.posts.find(post => post.tags.some(tag => tag.name === 'featured'));
+
+        if (featuredPost) {
+            const container = document.getElementById('featured-3');
             const html = `
                 <section class="featured featured--featured-3">
                     [ img ]
-                    <h3>tag</h3>
+                    <h3>${extractTag(featuredPost.tags)}</h3>
                     <h2>${featuredPost.title}</h2>
-                    <p>${extractText(featuredPost.summary)}</p>
+                    <p>${extractSummary(featuredPost.summary)}</p>
                 </section>
             `;
-
             container.innerHTML = html;
-        });
-    }
+        }
+    });
+}
 
+// ----------------------------------------------------------------------------------------------------
+
+export default class Blog extends PageManager {
     onReady() {
-        this.buildFeatured3();
+        buildFeatured3();
     }
 }

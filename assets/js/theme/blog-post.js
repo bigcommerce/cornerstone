b638050e4ad27e83fd66bcfaff4907d0b0b643dd
Samuel Reid <samjohnreid@gmail.com>
2022-08-11 17:04:50 -0400

    styling post page

--- assets/js/theme/blog-post.js
+++ assets/js/theme/blog-post.js
@@ -17,7 +17,7 @@ const injectTag = () => {
         
         const postsJson = JSON.parse(response);
         const tag = postsJson.post.tags.find(tag => postTypes.includes(tag.name.toLowerCase()));
-        const tagContainer = document.getElementById('blog-post-title');console.log(tagContainer);
+        const tagContainer = document.getElementById('blog-post-title');
 
         tagContainer.innerText = tag.name;
     });
